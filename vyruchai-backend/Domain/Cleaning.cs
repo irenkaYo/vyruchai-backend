81b15ab4c14eefcbb39f7786acb60c9f3e7d25e8
irenaYo <irenatugushi997@gmail.com>
2025-12-29 20:18:18 +0200

    added class Cleaning without Processing

---
+++ vyruchai-backend/Domain/Cleaning.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Cleaning : Request
+{
+    public decimal areaOfSquareMeter { get; private set; }
+    private decimal priceForOneSquareMeter = 8;
+    public override decimal CostCalculation(UrgencyLevel level)
+    {
+        return areaOfSquareMeter * priceForOneSquareMeter;
+    }
+
+    public override void Processing()
+    {
+
+    }
+}
