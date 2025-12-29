3c6eac014e0d59074b9a4f1abd9fc0d00593006c
irenaYo <irenatugushi997@gmail.com>
2025-12-29 20:38:31 +0200

    added class TechMaster

---
+++ vyruchai-backend/Domain/TechMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TechMaster : IMaster
+{
+    public MasterStatus Status { get; set; } = MasterStatus.IsFree;
+
+    public void TakeRequest()
+    {
+        Status = MasterStatus.IsBusy;
+    }
+
+    public void CompleteRequest()
+    {
+        Status = MasterStatus.IsBusy;
+    }
+
+}
+
