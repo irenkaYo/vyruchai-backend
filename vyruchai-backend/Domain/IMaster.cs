9aff41a9b81827f71cc539fd60848f42114a6f3f
irenaYo <irenatugushi997@gmail.com>
2025-12-29 20:23:57 +0200

    added interface IMaster

---
+++ vyruchai-backend/Domain/IMaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IMaster
+{
+    public MasterStatus status { get; set; }
+
+    public void TakeRequest();
+
+    public void CompleteRequest();
+}
