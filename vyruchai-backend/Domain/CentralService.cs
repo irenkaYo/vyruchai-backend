b75d44ff63a216824b86f98f385592b12040d11f
irenaYo <irenatugushi997@gmail.com>
2025-12-30 21:31:06 +0200

    added class CentralService

--- vyruchai-backend/Domain/CentralService.cs
+++ vyruchai-backend/Domain/CentralService.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vyruchai_backend.Domain
+public class CentralService
 {
-    internal class CentralService
+    private List<Request> Requests = new List<Request>();
+    private List<IMaster> Masters = new List<IMaster>();
+
+    public void CreateRequest(Request request)
     {
+        Requests.Add(request);
     }
+
+    public void CreateMaster(IMaster master)
+    {
+        Masters.Add(master);
+    }
+
+    public void TakeOneRequest(Request request, IMaster master)
+    {
+        if (master.CanHandle(request) == true)
+        {
+            request.AcceptedByMaster();
+        }
+        else
+        {
+            request.CancelRequest();
+        }
+    }
+
+    public void CompleteRequestByMaster(Request request, IMaster master)
+    {
+        master.CompleteRequest();
+        request.CompletedByMaster();
+    }
+    
 }
+
