55bdc9a1dca780827894fed6b9794cd71e1a8f1f
irenaYo <irenatugushi997@gmail.com>
2025-12-30 19:57:56 +0200

    added class Cleaner

---
+++ vyruchai-backend/Domain/Cleaner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Cleaner : IMaster
+{
+    public MasterStatus Status { get; set; }
+    public string Name { get; set; }
+
+    public Cleaner(string name)
+    {
+        Name = name;
+        Status = MasterStatus.IsFree;
+    }
+    public void TakeRequest()
+    {
+        Status = MasterStatus.IsBusy;
+    }
+
+    public void CompleteRequest()
+    {
+        Status = MasterStatus.IsFree;
+    }
+
+    public bool CanHandle(Request request)
+    {
+        if (request is Cleaning && Status == MasterStatus.IsFree)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
