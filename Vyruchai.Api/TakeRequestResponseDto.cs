97b40e884cd12ae519804494393ac14abbf26390
irenaYo <irenatugushi997@gmail.com>
2026-01-01 19:18:51 +0200

    done API and rework domain

---
+++ Vyruchai.Api/TakeRequestResponseDto.cs
@@ -0,0 +1,15 @@
+﻿
+public class TakeRequestResponseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Status { get; set; }
+
+    public TakeRequestResponseDto(Guid id, string name, RequestStatus status)
+    {
+        Id = id;
+        Name = name;
+        Status = status.ToString();
+    }
+}
+
